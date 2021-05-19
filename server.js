f1d7dd139a4eacce0297c238e93093e2092f8ad5
Dhruti Mistry <dhruti.g17@gmail.com>
2021-05-19 14:11:43 +0530

    complete project

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
+//import dotenv from 'dotenv';
 import postRoutes from './routers/posts.js';
 
 const app = express();
-dotenv.config();
+//dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -14,14 +14,14 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts',postRoutes);
-//const CONNECTION_URL = 'mongodb+srv://dhrutikmistry:mongodbkbm29it@cluster0.h8ptv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-app.get('/',(req,res) => {
+const CONNECTION_URL = 'mongodb+srv://dhrutikmistry:mongodbkbm29it@cluster0.h8ptv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+/*app.get('/',(req,res) => {
     res.send("Hello to memories API");
-})
+})*/
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`)))
     .catch((error) => console.log(error.message));
 
